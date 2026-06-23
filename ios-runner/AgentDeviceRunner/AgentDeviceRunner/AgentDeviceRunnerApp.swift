91111a69952b164b435c95ad4a1b3dbac89b379e
cryptotavares <joao.tavares@consensys.net>
2026-06-23 12:44:11 +0100

    feat(ios): add native XCUITest runner Xcode project

---
+++ ios-runner/AgentDeviceRunner/AgentDeviceRunner/AgentDeviceRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentDeviceRunnerApp.swift
+//  AgentDeviceRunner
+//
+//  Created by Joao Tavares on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AgentDeviceRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
